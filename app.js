5234ceaf2cda61507dbb116ed98e9bae5965a25e
rfirtermaiszter <rfirtermaiszter@performgroup.com>
2015-03-05 14:11:48 +0100

    upload files

---
+++ app.js
@@ -0,0 +1,7 @@
+var chalk = require('chalk');
+
+console.log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+));
